20ca724f10e96772c3327dadea516d8b9694f0ec
megdlawit anbessie <megdalenea@gmail.com>
2022-11-02 17:40:28 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,13 @@
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 200:
+    if sub_info.status_code >= 300:
         return 0
 
     return sub_info.json().get("data").get("subscribers")
