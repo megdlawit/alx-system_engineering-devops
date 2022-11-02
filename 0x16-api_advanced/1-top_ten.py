833f9b4b4480f75b5e0bb98a442128a7723a9214
megdlawit anbessie <megdalenea@gmail.com>
2022-11-02 18:04:39 +0300

    s

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def top_ten(subreddit):
+
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 400:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
